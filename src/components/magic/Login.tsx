d9b187cc2c3c5f769403a0b7df57fa402aea3dba
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2024-06-03 14:25:21 +0300

    pass props

--- src/components/magic/Login.tsx
+++ src/components/magic/Login.tsx
@@ -1,20 +1,19 @@
-import { LoginProps } from '@/utils/types'
-import Header from './Header'
+import { LoginProps } from "@/utils/types";
+import Header from "./Header";
 
-import EmailOTP from './auth/EmailOTP';
+import EmailOTP from "./auth/EmailOTP";
 
 const Login = ({ token, setToken }: LoginProps) => {
   return (
     <div className="login-page">
-      <Header />
-      <div className={`max-w-[100%] grid grid-cols-1 grid-flow-row auto-rows-fr gap-5 p-4 mt-8`}>
-      
-      
-        <EmailOTP token={token} setToken={setToken} />      
-  		
+      <Header token={token} setToken={setToken} />
+      <div
+        className={`max-w-[100%] grid grid-cols-1 grid-flow-row auto-rows-fr gap-5 p-4 mt-8`}
+      >
+        <EmailOTP token={token} setToken={setToken} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
