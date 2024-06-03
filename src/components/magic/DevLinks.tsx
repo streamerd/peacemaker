06d5bb9590c68145521335bdaf41784e39a2746a
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2024-06-03 14:48:43 +0300

    add links

--- src/components/magic/DevLinks.tsx
+++ src/components/magic/DevLinks.tsx
@@ -4,19 +4,19 @@ import Link from 'next/link';
 const DevLinks = ({ primary = false, footer = false }: { primary?: boolean; footer?: boolean }) => (
   <div className={`links ${footer ? 'footer-links' : ''}`}>
     <div className={`link ${primary ? 'text-[#6851ff]' : 'text-[#fff]'}`}>
-      <a href="https://magic.link/docs/home/welcome" target="_blank" rel="noreferrer">
+      <a href="https://aworldpeace.org" target="_blank" rel="noreferrer">
         Learn how
       </a>
     </div>
     <div className={`link-divider ${primary ? 'bg-[#DDDBE0]' : 'bg-[#a270d3]'}`} />
     <div className={`link ${primary ? 'text-[#6851ff]' : 'text-[#fff]'}`}>
-      <a href="https://dashboard.magic.link/signup" target="_blank" rel="noreferrer">
+      <a href="https://aworldpeace.org" target="_blank" rel="noreferrer">
         Proof of Peacemaking
       </a>
     </div>
     <div className={`link-divider ${primary ? 'bg-[#DDDBE0]' : 'bg-[#a270d3]'}`} />
     <div className={`link ${primary ? 'text-[#6851ff]' : 'text-[#fff]'}`}>
-      <a href="https://discord.gg/magiclabs" target="_blank" rel="noreferrer">
+      <a href="https://aworldpeace.org" target="_blank" rel="noreferrer">
         Proof of Recognition
       </a>
     </div>
