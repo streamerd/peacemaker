94f0ca7232a54b445c7eb969f17c1196c70c3e1d
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2024-06-03 14:27:22 +0300

    add typ

--- src/components/magic/cards/SendTransactionCard.tsx
+++ src/components/magic/cards/SendTransactionCard.tsx
@@ -46,11 +46,11 @@ const SendTransaction = () => {
     };
     web3.eth
       .sendTransaction(txnParams as any)
-      .on('transactionHash', (txHash) => {
+      .on('transactionHash', (txHash: any) => {
         setHash(txHash);
         console.log('Transaction hash:', txHash);
       })
-      .then((receipt) => {
+      .then((receipt: any) => {
         showToast({
           message: 'Transaction Successful',
           type: 'success',
@@ -59,7 +59,7 @@ const SendTransaction = () => {
         setAmount('');
         console.log('Transaction receipt:', receipt);
       })
-      .catch((error) => {
+      .catch((error: any) => {
         console.error(error);
         setDisabled(false);
       });
